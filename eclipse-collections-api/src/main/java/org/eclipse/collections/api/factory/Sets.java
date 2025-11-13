8ff38e147fe61583abdf02988840fc1fb6a278f4
Veda Vyasa <2400040217@kluniversity.in>
2025-11-13 13:42:27 +0530

    Fix JavaDoc: replace incorrect Set reference with SetIterable

--- eclipse-collections-api/src/main/java/org/eclipse/collections/api/factory/Sets.java
+++ eclipse-collections-api/src/main/java/org/eclipse/collections/api/factory/Sets.java
@@ -1,23 +1,6 @@
-/*
- * Copyright (c) 2022 Goldman Sachs and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * and Eclipse Distribution License v. 1.0 which accompany this distribution.
- * The Eclipse Public License is available at http://www.eclipse.org/legal/epl-v10.html
- * and the Eclipse Distribution License is available at
- * http://www.eclipse.org/org/documents/edl-v10.php.
- */
-
-package org.eclipse.collections.api.factory;
-
-import org.eclipse.collections.api.factory.set.FixedSizeSetFactory;
-import org.eclipse.collections.api.factory.set.ImmutableSetFactory;
-import org.eclipse.collections.api.factory.set.MultiReaderSetFactory;
-import org.eclipse.collections.api.factory.set.MutableSetFactory;
-
 /**
  * A factory class providing convenient static access to create various
- * types of {@link org.eclipse.collections.api.set.Set} and related set factories.
+ * types of {@link org.eclipse.collections.api.set.SetIterable} and related set factories.
  * <p>
  * This class serves as the main entry point for creating instances of:
  * <ul>
@@ -47,24 +30,3 @@ import org.eclipse.collections.api.factory.set.MutableSetFactory;
  *
  * @since 1.0
  */
-@SuppressWarnings("ConstantNamingConvention")
-@aQute.bnd.annotation.spi.ServiceConsumer(value = ImmutableSetFactory.class)
-@aQute.bnd.annotation.spi.ServiceConsumer(value = MutableSetFactory.class)
-@aQute.bnd.annotation.spi.ServiceConsumer(value = FixedSizeSetFactory.class)
-@aQute.bnd.annotation.spi.ServiceConsumer(value = MultiReaderSetFactory.class)
-public final class Sets
-{
-    public static final ImmutableSetFactory immutable =
-            ServiceLoaderUtils.loadServiceClass(ImmutableSetFactory.class);
-    public static final MutableSetFactory mutable =
-            ServiceLoaderUtils.loadServiceClass(MutableSetFactory.class);
-    public static final FixedSizeSetFactory fixedSize =
-            ServiceLoaderUtils.loadServiceClass(FixedSizeSetFactory.class);
-    public static final MultiReaderSetFactory multiReader =
-            ServiceLoaderUtils.loadServiceClass(MultiReaderSetFactory.class);
-
-    private Sets()
-    {
-        throw new AssertionError("Suppress default constructor for noninstantiability");
-    }
-}
