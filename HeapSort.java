a6d07b620f0092ac4eae91f91865c7597f966693
Mulla Arsiya Tasleem <2400090250@kluniversity.in>
2025-11-20 16:34:02 +0530

    Implement Heap Sort algorithm in HeapSort.java

---
+++ HeapSort.java
@@ -0,0 +1,67 @@
+import java.util.Scanner;
+
+public class HeapSort {
+
+    public void heapify(int arr[], int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
+
+        if (left < n && arr[left] > arr[largest])
+            largest = left;
+
+        if (right < n && arr[right] > arr[largest])
+            largest = right;
+
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+            heapify(arr, n, largest);
+        }
+    }
+
+    public void sort(int arr[]) {
+        int n = arr.length;
+
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        for (int i = n - 1; i >= 0; i--) {
+            int temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
+            heapify(arr, i, 0);
+        }
+    }
+
+    public void printArray(int arr[]) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.println("Enter " + n + " elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        HeapSort hs = new HeapSort();
+        System.out.println("Original array:");
+        hs.printArray(arr);
+
+        hs.sort(arr);
+
+        System.out.println("Sorted array using Heap Sort:");
+        hs.printArray(arr);
+
+        sc.close();
+    }
+}
