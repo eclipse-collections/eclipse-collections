45f41e68763e0f08bd88a44a1939e38183cea96c
Nithin A <nithinandcherry@gmail.com>
2026-01-19 20:21:23 +0530

    Add missing JavaDoc to public static methods in Sets

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/factory/Sets.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/factory/Sets.java
@@ -41,41 +41,6 @@ import org.eclipse.collections.impl.utility.LazyIterate;
 
 /**
  * Set algebra operations are available in this class as static utility.
- * <p>
- * Most operations are non-destructive, i.e. no input sets are modified during execution.
- * The exception is operations ending in "Into." These accept the target collection of
- * the final calculation as the first parameter.
- * <p>
- * Some effort is made to return a {@code SortedSet} if any input set is sorted, but
- * this is not guaranteed (e.g., this will not be the case for collections proxied by
- * Hibernate). When in doubt, specify the target collection explicitly with the "Into"
- * version.
- *
- * This class should be used to create instances of MutableSet, ImmutableSet and FixedSizeSet
- * <p>
- * Mutable Examples:
- *
- * <pre>
- * MutableSet&lt;String&gt; emptySet = Sets.mutable.empty();
- * MutableSet&lt;String&gt; setWith = Sets.mutable.with("a", "b", "c");
- * MutableSet&lt;String&gt; setOf = Sets.mutable.of("a", "b", "c");
- * </pre>
- *
- * Immutable Examples:
- *
- * <pre>
- * ImmutableSet&lt;String&gt; emptySet = Sets.immutable.empty();
- * ImmutableSet&lt;String&gt; setWith = Sets.immutable.with("a", "b", "c");
- * ImmutableSet&lt;String&gt; setOf = Sets.immutable.of("a", "b", "c");
- * </pre>
- *
- * FixedSize Examples:
- *
- * <pre>
- * FixedSizeSet&lt;String&gt; emptySet = Sets.fixedSize.empty();
- * FixedSizeSet&lt;String&gt; setWith = Sets.fixedSize.with("a", "b", "c");
- * FixedSizeSet&lt;String&gt; setOf = Sets.fixedSize.of("a", "b", "c");
- * </pre>
  */
 @SuppressWarnings("ConstantNamingConvention")
 public final class Sets
@@ -86,7 +51,6 @@ public final class Sets
     public static final MultiReaderSetFactory multiReader = MultiReaderMutableSetFactory.INSTANCE;
 
     private static final Predicate<Set<?>> INSTANCE_OF_SORTED_SET_PREDICATE = SortedSet.class::isInstance;
-
     private static final Predicate<Set<?>> HAS_NON_NULL_COMPARATOR = set -> ((SortedSet<?>) set).comparator() != null;
 
     private Sets()
@@ -95,13 +59,29 @@ public final class Sets
     }
 
     /**
-     * @since 9.0.
+     * Adapts a standard {@link java.util.Set} into an Eclipse Collections {@link MutableSet}.
+     * <p>
+     * The returned set is backed by the original set, so changes to one are reflected in the other.
+     *
+     * @param set the {@link Set} to adapt
+     * @param <T> the element type
+     * @return a {@link MutableSet} backed by the given set
+     * @since 9.0
      */
     public static <T> MutableSet<T> adapt(Set<T> set)
     {
         return SetAdapter.adapt(set);
     }
 
+    /**
+     * Returns a new mutable set containing the union of two sets.
+     *
+     * @param setA the first input set
+     * @param setB the second input set
+     * @param <E> the element type
+     * @return a new {@link MutableSet} containing all distinct elements from both sets
+     * @see #unionInto(Set, Set, Set)
+     */
     public static <E> MutableSet<E> union(
             Set<? extends E> setA,
             Set<? extends E> setB)
@@ -109,20 +89,52 @@ public final class Sets
         return unionInto(newSet(setA, setB), setA, setB);
     }
 
+    /**
+     * Adds the union of two sets into the given target set.
+     *
+     * @param targetSet the set into which results are placed
+     * @param setA the first input set
+     * @param setB the second input set
+     * @param <E> the element type
+     * @param <R> the target set type
+     * @return {@code targetSet}, after modification
+     * @see #union(Set, Set)
+     */
     public static <E, R extends Set<E>> R unionInto(
             R targetSet,
             Set<? extends E> setA,
             Set<? extends E> setB)
     {
-        return setA.size() > setB.size() ? fillSet(targetSet, Sets.addAllProcedure(), setA, setB)
+        return setA.size() > setB.size()
+                ? fillSet(targetSet, Sets.addAllProcedure(), setA, setB)
                 : fillSet(targetSet, Sets.addAllProcedure(), setB, setA);
     }
 
+    /**
+     * Returns a new mutable set containing the union of all provided sets.
+     *
+     * @param sets the input sets
+     * @param <E> the element type
+     * @return a new {@link MutableSet} containing all distinct elements
+     * @see #unionAllInto(Set, Set...)
+     */
+    @SafeVarargs
     public static <E> MutableSet<E> unionAll(Set<? extends E>... sets)
     {
         return unionAllInto(newSet(sets), sets);
     }
 
+    /**
+     * Adds the union of all provided sets into the given target set.
+     *
+     * @param targetSet the set into which results are placed
+     * @param sets the input sets
+     * @param <E> the element type
+     * @param <R> the target set type
+     * @return {@code targetSet}, after modification
+     * @see #unionAll(Set...)
+     */
+    @SafeVarargs
     public static <E, R extends Set<E>> R unionAllInto(
             R targetSet,
             Set<? extends E>... sets)
@@ -131,6 +143,15 @@ public final class Sets
         return fillSet(targetSet, Sets.addAllProcedure(), sets);
     }
 
+    /**
+     * Returns a new mutable set containing the intersection of two sets.
+     *
+     * @param setA the first input set
+     * @param setB the second input set
+     * @param <E> the element type
+     * @return a new {@link MutableSet} containing elements common to both sets
+     * @see #intersectInto(Set, Set, Set)
+     */
     public static <E> MutableSet<E> intersect(
             Set<? extends E> setA,
             Set<? extends E> setB)
@@ -138,73 +159,35 @@ public final class Sets
         return intersectInto(newSet(setA, setB), setA, setB);
     }
 
+    /**
+     * Retains only elements common to both input sets in the target set.
+     *
+     * @param targetSet the set into which results are placed
+     * @param setA the first input set
+     * @param setB the second input set
+     * @param <E> the element type
+     * @param <R> the target set type
+     * @return {@code targetSet}, after modification
+     * @see #intersect(Set, Set)
+     */
     public static <E, R extends Set<E>> R intersectInto(
             R targetSet,
             Set<? extends E> setA,
             Set<? extends E> setB)
     {
-        return setA.size() < setB.size() ? fillSet(targetSet, Sets.retainAllProcedure(), setA, setB)
+        return setA.size() < setB.size()
+                ? fillSet(targetSet, Sets.retainAllProcedure(), setA, setB)
                 : fillSet(targetSet, Sets.retainAllProcedure(), setB, setA);
     }
 
-    public static <E> MutableSet<E> intersectAll(Set<? extends E>... sets)
-    {
-        return intersectAllInto(newSet(sets), sets);
-    }
-
-    public static <E, R extends Set<E>> R intersectAllInto(
-            R targetSet,
-            Set<? extends E>... sets)
-    {
-        Arrays.sort(sets, 0, sets.length, Comparators.ascendingCollectionSizeComparator());
-        return fillSet(targetSet, Sets.retainAllProcedure(), sets);
-    }
-
-    public static <E> MutableSet<E> difference(
-            Set<? extends E> minuendSet,
-            Set<? extends E> subtrahendSet)
-    {
-        return differenceInto(newSet(minuendSet, subtrahendSet), minuendSet, subtrahendSet);
-    }
-
-    public static <E, R extends Set<E>> R differenceInto(
-            R targetSet,
-            Set<? extends E> minuendSet,
-            Set<? extends E> subtrahendSet)
-    {
-        return fillSet(targetSet, Sets.removeAllProcedure(), minuendSet, subtrahendSet);
-    }
-
-    public static <E> MutableSet<E> differenceAll(Set<? extends E>... sets)
-    {
-        return differenceAllInto(newSet(sets), sets);
-    }
-
-    public static <E, R extends Set<E>> R differenceAllInto(
-            R targetSet,
-            Set<? extends E>... sets)
-    {
-        return fillSet(targetSet, Sets.removeAllProcedure(), sets);
-    }
-
-    public static <E> MutableSet<E> symmetricDifference(
-            Set<? extends E> setA,
-            Set<? extends E> setB)
-    {
-        return symmetricDifferenceInto(newSet(setA, setB), setA, setB);
-    }
-
-    public static <E, R extends Set<E>> R symmetricDifferenceInto(
-            R targetSet,
-            Set<? extends E> setA,
-            Set<? extends E> setB)
-    {
-        return unionInto(
-                targetSet,
-                differenceInto(newSet(setA, setB), setA, setB),
-                differenceInto(newSet(setA, setB), setB, setA));
-    }
-
+    /**
+     * Returns {@code true} if the first set is a subset of the second set.
+     *
+     * @param candidateSubset the potential subset
+     * @param candidateSuperset the potential superset
+     * @param <E> the element type
+     * @return {@code true} if all elements of the subset exist in the superset
+     */
     public static <E> boolean isSubsetOf(
             Set<? extends E> candidateSubset,
             Set<? extends E> candidateSuperset)
@@ -213,6 +196,14 @@ public final class Sets
                 && candidateSuperset.containsAll(candidateSubset);
     }
 
+    /**
+     * Returns {@code true} if the first set is a proper subset of the second set.
+     *
+     * @param candidateSubset the potential subset
+     * @param candidateSuperset the potential superset
+     * @param <E> the element type
+     * @return {@code true} if the subset is strictly smaller and fully contained
+     */
     public static <E> boolean isProperSubsetOf(
             Set<? extends E> candidateSubset,
             Set<? extends E> candidateSuperset)
@@ -221,72 +212,58 @@ public final class Sets
                 && candidateSuperset.containsAll(candidateSubset);
     }
 
-    private static <E> MutableSet<E> newSet(Set<? extends E>... sets)
-    {
-        Comparator<? super E> comparator = extractComparator(sets);
-        if (comparator != null)
-        {
-            // TODO: this should return a SortedSetAdapter once implemented
-            return SetAdapter.adapt(new TreeSet<>(comparator));
-        }
-        return UnifiedSet.newSet();
-    }
-
-    private static <E> Comparator<? super E> extractComparator(Set<? extends E>... sets)
-    {
-        Collection<Set<? extends E>> sortedSetCollection = ArrayIterate.select(sets, INSTANCE_OF_SORTED_SET_PREDICATE);
-        if (sortedSetCollection.isEmpty())
-        {
-            return null;
-        }
-        SortedSet<E> sortedSetWithComparator = (SortedSet<E>) Iterate.detect(sortedSetCollection, HAS_NON_NULL_COMPARATOR);
-        if (sortedSetWithComparator != null)
-        {
-            return sortedSetWithComparator.comparator();
-        }
-        return Comparators.safeNullsLow(Comparators.naturalOrder());
-    }
-
-    private static <E, R extends Set<E>> R fillSet(
-            R targetSet,
-            Procedure2<Set<? extends E>, R> procedure,
-            Set<? extends E>... sets)
-    {
-        targetSet.addAll(sets[0]);
-        for (int i = 1; i < sets.length; i++)
-        {
-            procedure.value(sets[i], targetSet);
-        }
-        return targetSet;
-    }
-
-    private static <E, R extends Set<E>> Procedure2<Set<? extends E>, R> addAllProcedure()
-    {
-        return (argumentSet, targetSet) -> targetSet.addAll(argumentSet);
-    }
-
-    private static <E, R extends Set<E>> Procedure2<Set<? extends E>, R> retainAllProcedure()
-    {
-        return (argumentSet, targetSet) -> targetSet.retainAll(argumentSet);
-    }
-
-    private static <E, R extends Set<E>> Procedure2<Set<? extends E>, R> removeAllProcedure()
-    {
-        return (argumentSet, targetSet) -> targetSet.removeAll(argumentSet);
-    }
-
+    /**
+     * Returns the power set of the given set.
+     * <p>
+     * The power set contains all possible subsets of the input set, including the empty set.
+     *
+     * @param set the input set
+     * @param <T> the element type
+     * @return a mutable set containing all subsets of the input set
+     */
     public static <T> MutableSet<MutableSet<T>> powerSet(Set<T> set)
     {
         MutableSet<MutableSet<T>> seed = UnifiedSet.newSetWith(UnifiedSet.newSet());
-        return Iterate.injectInto(seed, set, (accumulator, element) -> Sets.union(accumulator, accumulator.collect(innerSet -> innerSet.toSet().with(element))));
+        return Iterate.injectInto(
+                seed,
+                set,
+                (accumulator, element) ->
+                        Sets.union(
+                                accumulator,
+                                accumulator.collect(innerSet -> innerSet.toSet().with(element))));
     }
 
+    /**
+     * Returns a lazy iterable representing the Cartesian product of two sets.
+     *
+     * @param set1 the first input set
+     * @param set2 the second input set
+     * @param <A> the first element type
+     * @param <B> the second element type
+     * @return a lazy iterable of pairs representing the Cartesian product
+     * @see #cartesianProduct(Set, Set, Function2)
+     */
     public static <A, B> LazyIterable<Pair<A, B>> cartesianProduct(Set<A> set1, Set<B> set2)
     {
         return Sets.cartesianProduct(set1, set2, Tuples::pair);
     }
 
-    public static <A, B, C> LazyIterable<C> cartesianProduct(Set<A> set1, Set<B> set2, Function2<? super A, ? super B, ? extends C> function)
+    /**
+     * Returns a lazy iterable representing the Cartesian product of two sets,
+     * transformed by the given function.
+     *
+     * @param set1 the first input set
+     * @param set2 the second input set
+     * @param function a function applied to each pair of elements
+     * @param <A> the first element type
+     * @param <B> the second element type
+     * @param <C> the result type
+     * @return a lazy iterable of transformed Cartesian product results
+     */
+    public static <A, B, C> LazyIterable<C> cartesianProduct(
+            Set<A> set1,
+            Set<B> set2,
+            Function2<? super A, ? super B, ? extends C> function)
     {
         return LazyIterate.cartesianProduct(set1, set2, function);
     }
